8ea481cbdd13974a907b402ac186f52787a728a1
Tanvir <tanveer99ryan@gmail.com>
2025-05-29 21:35:25 +0600

    product data delete

--- index.js
+++ index.js
@@ -174,6 +174,14 @@ async function run() {
       res.send(result);
     });
 
+    // product data delete
+    app.delete('/product-data-delete/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // ----------------------------PRODUCTS COLLECTION END----------------------------------
 
     //  ----------------------------USER COLLECTION START-------------------------------------
